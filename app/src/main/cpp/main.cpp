4655fc059203284ec832eafc85618cfa81d40fc4
Your Name <your-email@example.com>
2025-09-16 16:43:30 +0330

    FIX: use direct SDL include paths without SDL2 prefix

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,7 +1,7 @@
 #include "SDL.h"
-#include "SDL2/SDL_image.h"
-#include "SDL2/SDL_mixer.h"
-#include "SDL2/SDL_ttf.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
+#include "SDL_ttf.h"
 #include <iostream>
 #include <vector>
 #include <cmath>
